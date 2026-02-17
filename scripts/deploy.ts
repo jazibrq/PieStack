cf7ed279137edd7ac933b33ced2b67a5da683cd4
jazib <jqureshi@purdue.edu>
2026-02-17 15:01:12 -0500

    Vercel deployment?

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,7 @@
-import { ethers } from "hardhat";
+import hre from "hardhat";
 
 async function main() {
+  const ethers = hre.ethers;
   const [deployer] = await ethers.getSigners();
   console.log("Deploying with account:", deployer.address);
 
